fdd5bc32b9e0dff8cde0b3c2b87825466d1e542a
duce <duce02@meituan.com>
2025-01-17 15:45:32 +0800

    feat: 打包配置

--- emeifeng-serve-web-starter/src/main/java/org/emeifeng/emeifengservewebstarter/Controller/Test.java
+++ emeifeng-serve-web-starter/src/main/java/org/emeifeng/emeifengservewebstarter/Controller/Test.java
@@ -1,12 +1,14 @@
 package org.emeifeng.emeifengservewebstarter.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class Test {
 
-    @GetMapping("/")
+    @GetMapping()
     public String test() {
         return "hello";
     }
